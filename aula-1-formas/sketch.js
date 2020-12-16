a448cdbabfe5df30ab26c563170eef8f1c19d59a
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2020-12-16 01:54:20 +0100

    aula-1-formas new forms add

--- aula-1-formas/sketch.js
+++ aula-1-formas/sketch.js
@@ -4,13 +4,31 @@ function setup() {
 }
 
 function draw() {
-  rect((windowWidth/20)*2 - 30,(windowHeight/17)*2, 30, 100,10);
-  rect((windowWidth/20)*4 - 80,(windowHeight/17)*3,50,200,5)
-  strokeWeight(5)
-  stroke(255)
-  line(0,0,windowWidth/2,windowHeight/2)
-  stroke(255,255,0)
-  line(windowWidth/2,windowHeight/2,windowWidth,windowHeight)
-  ellipse(windowWidth/2,windowHeight/2,100)
+  rect((windowWidth / 20) * 2 - 30, (windowHeight / 17) * 2, 30, 100, 10);
+  rect((windowWidth / 20) * 4 - 80, (windowHeight / 17) * 3, 50, 200, 5);
+  strokeWeight(5);
+  stroke(255);
+  line(0, 0, windowWidth / 2, windowHeight / 2);
+  stroke(255, 255, 0);
+  line(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
+  ellipse(windowWidth / 2, windowHeight / 2, 100);
+  rectMode(CORNER);
+  fill(255);
+  rect(25, 25, 50, 50); // Draw white rectangle using CORNER mode
 
+  rectMode(CORNERS);
+  fill(100);
+  rect(25, 25, 50, 50); // Draw gray rectanle using CORNERS mode
+
+  rectMode(RADIUS);
+  fill(255);
+  rect(50, 50, 30, 30); // Draw white rectangle using RADIUS mode
+
+  rectMode(CENTER);
+  fill(100);
+  rect(50, 50, 30, 30); // Draw gray rectangle using CENTER mode
+
+  triangle(-(windowWidth / 20) * 13 ,-(windowHeight / 17) * 9, 58, 20, 86,- (windowHeight / 17) * 9)
+  arc((windowWidth / 20) * 13 ,(windowHeight / 17) * 9, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  circle((windowWidth / 20) * 18 ,(windowHeight / 17) * 15, 20);
 }
