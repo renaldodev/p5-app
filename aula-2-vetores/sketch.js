a1608a832fa2230d5dd6772149ccb70a4560e3af
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2020-12-21 04:27:35 +0100

    a pointer add

--- aula-2-vetores/sketch.js
+++ aula-2-vetores/sketch.js
@@ -1,7 +1,34 @@
+globalThis.clicked = false;
+const v = [];
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(255)
+  stroke(0);
+  console.log(v);
+  strokeWeight(10);
+  v.map((vector) => {
+	//   vector.mult(random(1))
+    point(vector.x, vector.y);
+  });
+}
 
+function mousePressed(fxn) {
+  makePointer();
+}
+
+const makePointer = () => {
+  v.push(createVector(mouseX, mouseY));
+};
+
+const drowLine=(x0,y0,x,y)=>{
+	stroke(255,0,255)
+	if(x0==undefined){
+		x0=0;
+		y0=0
+	}
+	line(x0, y0, x, y)
 }
